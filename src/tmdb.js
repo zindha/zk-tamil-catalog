6b85dc4b91de6064db55211cbec88380859e2528
zindha <zindhak@gmail.com>
2026-02-14 16:22:00 +0530

    Update tmdb.js

--- src/tmdb.js
+++ src/tmdb.js
@@ -47,6 +47,7 @@ export class TMDBClient {
     }
   }
 
+  // Movies
   async getTopRated(page = 1) {
     const today = new Date().toISOString().split('T')[0];
     const url = `${TMDB_BASE}/discover/movie?api_key=${this.apiKey}&with_original_language=ta&sort_by=vote_average.desc&vote_count.gte=50&page=${page}&language=en-US&with_runtime.gte=40&release_date.lte=${today}&include_adult=false`;
@@ -95,6 +96,26 @@ export class TMDBClient {
     return this.fetchWithCache(url, `search_${query}_v2_p${page}`);
   }
 
+  // Series
+  async getSeriesTrending(page = 1) {
+    // Genre exclusions: 10764 (Reality), 10767 (Talk Show)
+    const url = `${TMDB_BASE}/trending/tv/week?api_key=${this.apiKey}&page=${page}&language=en-US&with_original_language=ta&without_genres=10764,10767`;
+    return this.fetchWithCache(url, `series_trending_ta_p${page}`);
+  }
+
+  async getSeriesPopular(page = 1) {
+    // Exclude Reality (10764) and Talk (10767)
+    const url = `${TMDB_BASE}/discover/tv?api_key=${this.apiKey}&with_original_language=ta&sort_by=popularity.desc&page=${page}&language=en-US&without_genres=10764,10767&vote_count.gte=5`;
+    return this.fetchWithCache(url, `series_popular_ta_p${page}`);
+  }
+
+  async getSeriesLatest(page = 1) {
+    // Exclude Reality (10764) and Talk (10767)
+    const today = new Date().toISOString().split('T')[0];
+    const url = `${TMDB_BASE}/discover/tv?api_key=${this.apiKey}&with_original_language=ta&sort_by=first_air_date.desc&page=${page}&language=en-US&first_air_date.lte=${today}&without_genres=10764,10767`;
+    return this.fetchWithCache(url, `series_latest_ta_p${page}`);
+  }
+
   convertToMeta(movie) {
     const meta = {
       id: `tmdb:${movie.id}`,
@@ -109,6 +130,27 @@ export class TMDBClient {
       description: movie.overview || 'No description available',
       releaseInfo: movie.release_date ? movie.release_date.split('-')[0] : undefined,
       imdbRating: movie.vote_average && movie.vote_average > 0 ? movie.vote_average.toFixed(1) : undefined
+    };
+    
+    Object.keys(meta).forEach(key => meta[key] === undefined && delete meta[key]);
+    
+    return meta;
+  }
+
+  convertToSeriesMeta(series) {
+    const meta = {
+      id: `tmdb:${series.id}`,
+      type: 'series',
+      name: series.name || series.original_name || 'Unknown',
+      poster: series.poster_path 
+        ? `https://image.tmdb.org/t/p/w500${series.poster_path}`
+        : undefined,
+      background: series.backdrop_path
+        ? `https://image.tmdb.org/t/p/original${series.backdrop_path}`
+        : undefined,
+      description: series.overview || 'No description available',
+      releaseInfo: series.first_air_date ? series.first_air_date.split('-')[0] : undefined,
+      imdbRating: series.vote_average && series.vote_average > 0 ? series.vote_average.toFixed(1) : undefined
     };
     
     Object.keys(meta).forEach(key => meta[key] === undefined && delete meta[key]);
